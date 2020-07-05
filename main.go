8664aab8f088290cdb3eda787138a72c5a3cb02f
Саша Воробьёв <avorobiev@deloitte.ru>
2020-07-05 10:34:15 +0300

    Реализовал лол сервер для фотоблогера

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 var tpl *template.Template
@@ -14,9 +23,84 @@ func init() {
 func main() {
 	http.HandleFunc("/", index)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	c := getCookie(w, req)
+
+	if req.Method == http.MethodPost {
+		f, fh, err := req.FormFile("ufile")
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer f.Close()
+
+		ext := strings.Split(fh.Filename, ".")[1]
+
+		fn := sha1.New()
+
+		io.Copy(fn, f)
+
+		filename := fmt.Sprintf("%x", fn.Sum(nil)) + "." + ext
+
+		path := filepath.Join(".", "public", "pics", filename)
+
+		nf, err := os.Create(path)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer nf.Close()
+		f.Seek(0, 0)
+
+		io.Copy(nf, f)
+
+		c = appendCookie(w, c, path)
+
+	}
+
+	xs := strings.Split(c.Value, "|")
+
+	xscorrect := xs[1:]
+
+	tpl.ExecuteTemplate(w, "index.gohtml", xscorrect)
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+
+	if err != nil {
+		sID, err := uuid.NewV4()
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+
+		http.SetCookie(w, c)
+	}
+
+	return c
+}
+
+func appendCookie(w http.ResponseWriter, c *http.Cookie, newfn string) *http.Cookie {
+	s := c.Value
+
+	if !strings.Contains(s, newfn) {
+		s += "|" + newfn
+	}
+
+	c.Value = s
+
+	http.SetCookie(w, c)
+
+	return c
 }
